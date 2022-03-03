66d6d991a61fdc6b04de525035c522473eefb36f
lio <joechipjoechip@gmail.com>
2022-03-03 15:14:46 +0100

    update db schema for homefolio and cases

---
+++ src/api/homefolio/controllers/homefolio.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  homefolio controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::homefolio.homefolio');
---
+++ src/api/homefolio/routes/homefolio.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * homefolio router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::homefolio.homefolio');
