f991c5c01c65aefeaf92df42eadaf9dc73dbda40
lio <joechipjoechip@gmail.com>
2022-03-09 17:04:17 +0100

    update plugin upload settings (but don't know if taken in consideration - need to investigate -

---
+++ config/plugin.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+	upload: {
+	  config: {
+		breakpoints: {
+		  xlarge: 1920,
+		  large: 1000,
+		  medium: 750,
+		  small: 500,
+		  thumbnail: 100,
+		  xsmall: 64,
+		  custom: 20,
+		},
+	  },
+	},
+  });
