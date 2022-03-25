1f751343e01dc9f6ea34b8e7c51a6e064f9142ec
lio <joechipjoechip@gmail.com>
2022-03-25 19:30:19 +0100

    remove strapi/nuxt module

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -47,14 +47,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: ['@nuxtjs/strapi'],
-
-  strapi: {
-    url: process.env.STRAPI_BASE_URL,
-    prefix: '/api',
-    version: 'v4',
-    cookie: {},
-  },
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
