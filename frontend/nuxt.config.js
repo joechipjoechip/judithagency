6d997f17e7091fca837a75c4e536db5134fa42aa
lio <joechipjoechip@gmail.com>
2022-04-04 15:08:57 +0200

    front : add v-view plugin

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -42,7 +42,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    "~/plugins/vue2-touch-events.js"
+    "~/plugins/vue2-touch-events.js",
+    "~/plugins/v-view.js",
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
